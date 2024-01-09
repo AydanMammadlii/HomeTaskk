816305abb4aa1735e8f4f08d0371a4ac7cbdd621
unknown <aydanmma@code.edu.az>
2024-01-09 12:22:15 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ClassLibrary;
+
+
+
+
+Product p1 = new("Aydan", 3500M);
+if (!File.Exists(@".\Test\test.txt"))
+{
+    File.Create(@".\Test\test.txt");
+}
+//File.Delete(@".\Test\test.txt");
+
+StreamWriter streamWriter = new StreamWriter(@".\Test\test.txt");
+streamWriter.WriteLine($"Name: {p1.Name}; Price: {p1.Price}");
+streamWriter.Close();
